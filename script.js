a317330e3074e35da343e0aeac04234862fdc8ba
ninotal90 <ntalakvadze90@gmail.com>
2023-05-09 11:54:51 +0400

    added html and javascript

---
+++ script.js
@@ -0,0 +1,103 @@
+`use strict`;
+
+1;
+// let array = [5, 25, 89, 120, 36];
+// array.push("javascript", "python");
+// array.unshift("html", "css");
+// console.log(array.length);
+// array.shift([0]);
+// array.pop([9]);
+// console.log(array);
+
+2;
+// let array1 = ["ფორთოხალი", "ბანანი", "მსხალი"];
+// array1.push("ვაშლი", "ანანასი");
+// array1.unshift("საზამთრო");
+// console.log(array1.length);
+// array1.splice(3, 0, "მანგო");
+// array1.shift();
+// array1.pop();
+// console.log(array1);
+// console.log(array1.length);
+
+3;
+// let array2 = [1, 2, 3, 4, 5];
+// array2.splice(3, 0, "a", "b", "c");
+// console.log(array2);
+
+4;
+// let sum = [1, 2, 3, 4, 5];
+// let index = 0;
+
+// sum.forEach(function (item) {
+//   index += item;
+// });
+// console.log(index);
+
+5;
+// let newNumber = 100;
+// for (let i = 100; i > 0; i--) {
+//   console.log(i);
+// }
+
+6;
+// let number = function (num1, num2) {
+//   if (num1 > num2) {
+//     console.log("num1 is the largest");
+//   } else if (num2 > num1) {
+//     console.log("num2 is the largest");
+//   } else {
+//     console.log("error");
+//   }
+// };
+// number(10, 20);
+// console.log(number);
+
+7;
+// let numbers = [12, 25, 3, 6, 8, 14, 7, 23];
+// let numbersResult = numbers.map(function(x){
+//     console.log(x / 3);
+// })
+
+8;
+// let string = ["html", "css", "javascript", "python", "php"];
+// let stringResult = string.filter(function (y) {
+//   return y.length > 3;
+// });
+// console.log(stringResult);
+
+9;
+// let words = ["Madrid", "Rome", "Milan", "Berlin"];
+// let wordsResult = words.filter(function (item1) {
+//   return item1.includes("M") || ('m');
+// });
+// console.log(wordsResult);
+
+10;
+// let arr1 = [1, 2];
+// let arr2 = [3, 4];
+// let arr3 = [5, 6];
+
+// let arr123 = arr1.concat(arr2, arr3);
+// console.log(arr123);
+
+11;
+// let arr = [-1, -2, -3, 4];
+// let numberSome = (item) => item > 0;
+// let resultSome = arr.some(numberSome);
+// console.log(resultSome);
+
+// let arr = [-1, -2, -3, 4].some(function (x) {
+//   return x > 0;
+// });
+// console.log(arr);
+
+// let arr = [-1, -2, -3, 4].some((d) => d > 0);
+// console.log(arr);
+
+12;
+// let numb = [2, 15, 10, 24];
+// numb.splice(2, 1);
+// console.log(numb);
+
+
